ac350af863a88afefd67cbb8b1559702caac7cea
Mark Cooper <markchristophercooper@gmail.com>
2011-11-16 17:08:34 -0800

    Major reworking - new classes

--- lib/overdrive_metadata.rb
+++ lib/overdrive_metadata.rb
@@ -27,7 +27,8 @@ class OverdriveMetadata
   	attr_reader :records
 
     GMD      = '[electronic resource]'
-    ISBN_AUD = '(sound recording : OverDrive Audio Book)'
+    FORMAT   = 'eBook'
+    ISBN_EBK = '(electronic bk. : OverDrive Electronic Book)'
     OD_URL   = 'http://www.overdrive.com'
     AGENCY   = 'JTH' # Sonoma County Library
     ACCESS   = 'Mode of access: World Wide Web.'
@@ -77,133 +78,144 @@ class OverdriveMetadata
   		@metadata.each do |row|
     		@records << create_record(row)
   		end
-      @records.compact!
-      merge_by_isbn
+      @records.compact
+      # @records.compact!
+      # merge_by_isbn
   	end
 
-    def merge_by_isbn
-      isbns = Hash.new(0)
-      @records.each do |record|
-        isbns[record['020'].value] += 1 if record['020']
-      end
-      isbns.delete_if { |k,v| v < 2 }
-      isbns.keys.each do |isbn|
-        rcds = @records.find_all { |r| r['020']['a'] == isbn if r['020'] }
-        raise 'Found invalid number of duplicate records: ' + isbn unless rcds.size == 2
-        file_note = rcds[1].find { |f| f.tag == '500' and f['a'] =~ /OverDrive (WMA|MP3) Audiobook/ }
-        excerpt   = rcds[1].find { |f| f.tag == '856' and f['y'] =~ /Excerpt/ }
-        raise 'Unable to identify format note and excerpt: ' + isbn unless file_note and excerpt
-        rcds[0].fields.insert(rcds[0].fields.index { |f| f.tag == '500' }, file_note)
-        rcds[0].fields.insert(rcds[0].fields.index { |f| f.tag == '856' and f['y'] =~ /Excerpt/ }, excerpt)
-        @records.delete rcds[1]
-      end
-      @records
-    end
+    # def merge_by_isbn
+    #   isbns = Hash.new(0)
+    #   @records.each do |record|
+    #     isbns[record['020'].value] += 1 if record['020']
+    #   end
+    #   isbns.delete_if { |k,v| v < 2 }
+    #   isbns.keys.each do |isbn|
+    #     rcds = @records.find_all { |r| r['020']['a'] == isbn if r['020'] }
+    #     raise 'Found invalid number of duplicate records: ' + isbn unless rcds.size == 2
+    #     file_note = rcds[1].find { |f| f.tag == '500' and f['a'] =~ /OverDrive (WMA|MP3) Audiobook/ }
+    #     excerpt   = rcds[1].find { |f| f.tag == '856' and f['y'] =~ /Excerpt/ }
+    #     raise 'Unable to identify format note and excerpt: ' + isbn unless file_note and excerpt
+    #     rcds[0].fields.insert(rcds[0].fields.index { |f| f.tag == '500' }, file_note)
+    #     rcds[0].fields.insert(rcds[0].fields.index { |f| f.tag == '856' and f['y'] =~ /Excerpt/ }, excerpt)
+    #     @records.delete rcds[1]
+    #   end
+    #   @records
+    # end
 
   	def create_record(data)
-  		isbn             = data[HEADERS[:isbn]]
-      date             = data[HEADERS[:date]]
-      place            = data[HEADERS[:place]]
-      publisher        = data[HEADERS[:publisher]]
-      year             = month = day = ''
-      if date.match(/\d{1,2}\/\d{1,2}\/\d{4}/)
-        month, day, year = date.split '/'
-      end
-      year             = date.match(/\d{4}/).to_s # Fall-back
-      time             = data[HEADERS[:time]]
-      hr, mn, sc       = time.split ':'
-      author           = @coder.decode(data[HEADERS[:author]])
-      title            = @coder.decode(data[HEADERS[:title]])
-      title_src        = data[HEADERS[:title_src]]
-      reader           = @coder.decode(data[HEADERS[:reader]])
-      requires         = data[HEADERS[:requires]]
-      format           = data[HEADERS[:format]]
-      filesize         = kb_to_mb(data[HEADERS[:filesize]])
-      summary          = Sanitize.clean(@coder.decode(data[HEADERS[:summary]])).gsub(/\s{2}+/, '').strip
-      subjects         = data[HEADERS[:subjects]].split ','
-      litf             = subjects.include?('Fiction') ? 'f' : ' '
-      download         = data[HEADERS[:download]]
-      excerpt          = data[HEADERS[:excerpt]]
-      thumb            = data[HEADERS[:thumb]]
-      cover            = data[HEADERS[:cover]]
-      oclc             = data[HEADERS[:oclc]].to_s.empty? ? 'ovr' + make_id(data[HEADERS[:filesize]], mn, sc) : 'ocn' + data[HEADERS[:oclc]]
-      
-      begin
-        fields = []
-    		fields << make_control_field('001', oclc)
-        fields << make_control_field('006', 'm        h        ')
-        fields << make_control_field('007', 'sz usnnnnnnned')
-        fields << make_control_field('007', 'cr nna   |||||')
-        fields << make_control_field('008', make_fixed_field(year, month, day, litf))
+  		field = package_data(data)
+      record = format.match(/#{FORMAT}/) ? OverdriveMetadata::Ebook.new : OverdriveMetadata::AudioBook.new
 
-    		fields << make_data_field('020', ' ', ' ', isbn + ' ' + ISBN_AUD) unless isbn.empty?
-        fields << make_acq_src(OD_URL)
-        fields << make_source(AGENCY)
-    		
-    		fields << make_data_field('100', '1', ' ', normalize_author(author))
-        fields << make_title(title, author)
-        fields << make_publication(place, publisher, year)
-        fields << make_physical(hr, mn)
+      # begin
+      #   fields = []
+      #   fields << make_control_field('001', data[:oclc])
+      #   fields << make_control_field('006', 'm        h        ')
+      #   fields << make_control_field('007', 'sz usnnnnnnned')
+      #   fields << make_control_field('007', 'cr nna   |||||')
+      #   fields << make_control_field('008', make_fixed_field(year, month, day, litf))
 
-        fields << make_data_field('306', ' ', ' ', hr + mn + sc)
-        fields << make_data_field('538', ' ', ' ', ACCESS)
-        fields << make_data_field('538', ' ', ' ', 'Requires ' + requires + '.')
-        fields << make_data_field('500', ' ', ' ', format + ' (file size: ' + filesize + ' MB).')
-        fields << make_data_field('511', '0', ' ', 'Read by ' + reader + '.') unless reader.empty? 
-        fields << make_data_field('520', ' ', ' ', summary) unless summary.match(/^#+$/)
+      #   fields << make_data_field('020', ' ', ' ', data[:isbn] + ' ' + ISBN_AUD) unless data[:isbn].empty?
+      #   fields << make_acq_src(OD_URL)
+      #   fields << make_source(AGENCY)
+        
+      #   fields << make_data_field('100', '1', ' ', normalize_author(author))
+      #   fields << make_title(title, author)
+      #   fields << make_publication(place, publisher, year)
+      #   fields << make_physical(hr, mn)
 
-        fields << make_data_field('500', ' ', ' ', 'Title from: ' + title_src + '.')
-        fields << make_data_field('500', ' ', ' ', 'Unabridged.')
-        fields << make_data_field('500', ' ', ' ', 'Duration: ' + hr + ' hr., ' + mn + ' min.')
+      #   fields << make_data_field('306', ' ', ' ', hr + mn + sc)
+      #   fields << make_data_field('538', ' ', ' ', ACCESS)
+      #   fields << make_data_field('538', ' ', ' ', 'Requires ' + requires + '.')
+      #   fields << make_data_field('500', ' ', ' ', format + ' (file size: ' + filesize + ' MB).')
+      #   fields << make_data_field('511', '0', ' ', 'Read by ' + reader + '.') unless reader.empty? 
+      #   fields << make_data_field('520', ' ', ' ', summary) unless summary.match(/^#+$/)
 
-        subjects.each { |s| fields << make_subject(@coder.decode(s)) }
-        fields << make_subject(DOWN_SH)
+      #   fields << make_data_field('500', ' ', ' ', 'Title from: ' + title_src + '.')
+      #   fields << make_data_field('500', ' ', ' ', 'Unabridged.')
+      #   fields << make_data_field('500', ' ', ' ', 'Duration: ' + hr + ' hr., ' + mn + ' min.')
 
-        fields << make_data_field('700', '1', ' ', normalize_author(reader))
+      #   subjects.each { |s| fields << make_subject(@coder.decode(s)) }
+      #   fields << make_subject(DOWN_SH)
 
-        fields << make_link(download, URL_MSG)
-        fields << make_link(excerpt, 'Excerpt (' + format + ').')
-        fields << make_img_link(title, cover, thumb)
+      #   fields << make_data_field('700', '1', ' ', normalize_author(reader))
 
-        fields << make_data_field('907', ' ', ' ', 'ER')
-        fields << make_data_field('991', ' ', ' ', DISCLAIM)
+      #   fields << make_link(download, URL_MSG)
+      #   fields << make_link(excerpt, 'Excerpt (' + format + ').')
+      #   fields << make_img_link(title, cover, thumb)
 
-    		fields.compact! # remove nil fields
-    		
-        record = MARC::Record.new
-        fields.each { |f| record.append f } # add fields to record
+      #   fields << make_data_field('907', ' ', ' ', 'ER')
+      #   fields << make_data_field('991', ' ', ' ', DISCLAIM)
 
-        # leader - accept hash in future
-        ldr = record.leader
-        ldr[5]  = 'n'
-        ldr[6]  = 'i'
-        ldr[7]  = 'm'
-        ldr[17] = 'M'
-        ldr[18] = 'a'
+      #   fields.compact! # remove nil fields
+        
+      #   record = MARC::Record.new
+      #   fields.each { |f| record.append f } # add fields to record
 
-    		return record
-      rescue Exception => ex
-        puts 'ERROR: ' + ex.message + ' for: ' + title
-        return nil
-      end
+      #   # leader - accept hash in future
+      #   ldr = record.leader
+      #   ldr[5]  = 'n'
+      #   ldr[6]  = 'i'
+      #   ldr[7]  = 'm'
+      #   ldr[17] = 'M'
+      #   ldr[18] = 'a'
+
+      #   return record
+      # rescue Exception => ex
+      #   puts 'ERROR: ' + ex.message + ' for: ' + title
+      #   return nil
+      # end
   	end
 
-    ##
-    # Generate a id no.
-
-    def make_id(*values)
-      return values.join
+    def package_data(data)
+      values = {}
+      values[:isbn]             = data[HEADERS[:isbn]]
+      values[:date]             = data[HEADERS[:date]]
+      values[:place]            = data[HEADERS[:place]]
+      values[:publisher]        = data[HEADERS[:publisher]]
+      values[:month]            = ''
+      values[:day]              = ''
+      if date.match(/\d{1,2}\/\d{1,2}\/\d{4}/)
+        month, day, year        = date.split '/'
+        values[:month]          = month
+        values[:day]            = day
+      end
+      values[:year]             = date.match(/\d{4}/).to_s # Fall-back
+      values[:time]             = data[HEADERS[:time]]
+      hr, mn, sc                = time.split ':'
+      values[:hours]            = hr
+      values[:minutes]          = mn
+      values[:seconds]          = sc
+      values[:author]           = @coder.decode(data[HEADERS[:author]])
+      values[:title]            = @coder.decode(data[HEADERS[:title]])
+      values[:title_src]        = data[HEADERS[:title_src]]
+      values[:reader]           = @coder.decode(data[HEADERS[:reader]])
+      values[:requires]         = data[HEADERS[:requires]]
+      values[:format]           = data[HEADERS[:format]]
+      values[:filesize]         = kb_to_mb(data[HEADERS[:filesize]])
+      values[:summary]          = Sanitize.clean(@coder.decode(data[HEADERS[:summary]])).gsub(/\s{2}+/, '').strip
+      values[:subjects]         = data[HEADERS[:subjects]].split ','
+      values[:litf]             = subjects.include?('Fiction') ? 'f' : ' '
+      values[:download]         = data[HEADERS[:download]]
+      values[:excerpt]          = data[HEADERS[:excerpt]]
+      values[:thumb]            = data[HEADERS[:thumb]]
+      values[:cover]            = data[HEADERS[:cover]]
+      values[:oclc]             = data[HEADERS[:oclc]].to_s.empty? ? 'ovr' + make_id(data[HEADERS[:filesize]], mn, sc) : 'ocn' + data[HEADERS[:oclc]]
+      return values
     end
 
-  	def make_control_field(tag, value)
-  		return nil if value.empty?
-  		return MARC::ControlField.new(tag, value)
-  	end
+   #  def make_id(*values)
+   #    return values.join
+   #  end
 
-  	def make_data_field(tag, ind1, ind2, value, code = 'a')
-  		return nil if value.empty?
-  		return MARC::DataField.new(tag, ind1, ind2, [code, value])
-  	end
+  	# def make_control_field(tag, value)
+  	# 	return nil if value.empty?
+  	# 	return MARC::ControlField.new(tag, value)
+  	# end
+
+  	# def make_data_field(tag, ind1, ind2, value, code = 'a')
+  	# 	return nil if value.empty?
+  	# 	return MARC::DataField.new(tag, ind1, ind2, [code, value])
+  	# end
 
     def make_fixed_field(year, month, day, litf = ' ')
       raise DATE_ERR if year.empty?
@@ -315,6 +327,48 @@ class OverdriveMetadata
 
     def kb_to_mb(size)
       return (size.to_f / 1024).to_i.to_s
+    end
+
+    class Record
+
+      attr_reader :fields
+
+      def initialize
+        @fields = []
+      end
+
+      def make_id(*values)
+        return values.join
+      end
+
+      def make_control_field(tag, value)
+        return nil if value.empty?
+        return MARC::ControlField.new(tag, value)
+      end
+
+      def make_data_field(tag, ind1, ind2, value, code = 'a')
+        return nil if value.empty?
+        return MARC::DataField.new(tag, ind1, ind2, [code, value])
+      end
+
+    end
+
+    class EBook < Record
+
+      def initialize
+        super
+      end
+      
+    end
+
+    class AudioBook < Record
+
+      ISBN_AUD = '(sound recording : OverDrive Audio Book)'
+
+      def initialize
+        super
+      end
+      
     end
 
 end
